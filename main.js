f871fc4988686a441fe6ae2baf5a952817ca9a70
riquelmeamorimgalindo <106089105+riquelmeamorimgalindo@users.noreply.github.com>
2023-11-08 11:38:53 -0300

    67676

--- main.js
+++ main.js
@@ -3,3 +3,51 @@ function tocaSomAplausos(){
 }
 
 document.querySelector('tecla_aplausos').onclick=tocaSomAplausos;
+
+function tocaSomVaia(){
+    document.querySelector ('# som_tecla_vaia').play();
+}
+
+document.querySelector('tecla_vaia').onclick=tocaSomVaia;
+
+function tocaSomRisadas(){
+    document.querySelector ('# som_tecla_risada').play();
+}
+
+document.querySelector('tecla_risada').onclick=tocaSomRisadas;
+
+function tocaSomErrado(){
+    document.querySelector ('# som_tecla_errado').play();
+}
+
+document.querySelector('tecla_errado').onclick=tocaSomErrado;
+
+function tocaSomGrilo(){
+    document.querySelector ('# som_tecla_grilo').play();
+}
+
+document.querySelector('tecla_grilo').onclick=tocaSomGrilo;
+
+function tocaSomTambores(){
+    document.querySelector ('# som_tecla_tambores').play();
+}
+
+document.querySelector('tecla_tambores').onclick=tocaSomTambores;
+
+function tocaSomTrombeta(){
+    document.querySelector ('# som_tecla_trombeta').play();
+}
+
+document.querySelector('tecla_trombeta').onclick=tocaSomTrombeta;
+
+function tocaSomDinheiro(){
+    document.querySelector ('# som_tecla_dinheiro').play();
+}
+
+document.querySelector('tecla_dinheiro').onclick=tocaSomDinheiro;
+
+function tocaSomVitoria(){
+    document.querySelector ('# som_tecla_vitoria').play();
+}
+
+document.querySelector('tecla_vitoria').onclick=tocaSomVitoria;
